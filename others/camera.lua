936d91995d3a9f09779ef9390c7d621566b3a062
Program <67320703+Program132@users.noreply.github.com>
2022-03-05 18:48:20 +0100

    Camera

---
+++ others/camera.lua
@@ -0,0 +1,9 @@
+------------------ BY PROGRAM (Program Entertainment) ------------------
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+local camera = worksapce.CurrentCamera
+local TargetCamera = workspace.Cam -- My camera
+
+camera.CameraType = Enum.CameraType.Scriptable
+camera.CFrame = TargetCamera.CFrame
